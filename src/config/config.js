afef3a8f690213870637f51e66f5a19dccd46cae
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-09-02 16:03:19 -0700

    change api url:

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const BACKEND_API = "https://pacman-backend-aap1.onrender.com/api/pacman";
+const BACKEND_API = "https://pacman-backend-c61f.onrender.com/api/pacman";
 // const BACKEND_API = "http://localhost:7777/api/pacman";
 
 export { BACKEND_API };
